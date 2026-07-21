ca2b5165123fb3d99da191c748b46629d7781da2
Marcos-70 <marcoscato70@gmail.com>
2026-07-21 09:08:57 +0100

    fix: schema.sql com tratamento de erros + remover config problematica
    - schema.sql: DO BEGIN...EXCEPTION WHEN OTHERS THEN END para ignorar erros
    - Removido spring.web.resources.add-mappings=false (causava problemas)
    - Removido spring.mvc.throw-exception-if-no-handler-found=true
    - Adicionado hikari connection-timeout e maximum-pool-size

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,7 +1,7 @@
--- Alter text columns that were created as VARCHAR(255) by Hibernate
-ALTER TABLE schools ALTER COLUMN logo TYPE text;
-ALTER TABLE schools ALTER COLUMN motto TYPE text;
-ALTER TABLE users ALTER COLUMN avatar TYPE text;
-ALTER TABLE students ALTER COLUMN photo TYPE text;
-ALTER TABLE teachers ALTER COLUMN photo TYPE text;
-ALTER TABLE enrollments ALTER COLUMN photo TYPE text;
+-- Alter text columns (ignore errors if already altered)
+DO $$ BEGIN ALTER TABLE schools ALTER COLUMN logo TYPE text; EXCEPTION WHEN OTHERS THEN END; $$;
+DO $$ BEGIN ALTER TABLE schools ALTER COLUMN motto TYPE text; EXCEPTION WHEN OTHERS THEN END; $$;
+DO $$ BEGIN ALTER TABLE users ALTER COLUMN avatar TYPE text; EXCEPTION WHEN OTHERS THEN END; $$;
+DO $$ BEGIN ALTER TABLE students ALTER COLUMN photo TYPE text; EXCEPTION WHEN OTHERS THEN END; $$;
+DO $$ BEGIN ALTER TABLE teachers ALTER COLUMN photo TYPE text; EXCEPTION WHEN OTHERS THEN END; $$;
+DO $$ BEGIN ALTER TABLE enrollments ALTER COLUMN photo TYPE text; EXCEPTION WHEN OTHERS THEN END; $$;
